462256e06e370107c12e29c4b8622dac439dab2a
Simret <simruineb@gmail.com>
2022-04-11 08:42:23 -0700

    Combinations of numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -17,15 +17,12 @@ int main(void)
 
 
 		while (c < 10)
-
 		{
 
 			putchar(48 + c);
 			if (c != 9)
-
 			{
-																putchar(',');									
-			    putchar(' ');
+																putchar(',');											    putchar(' ');
 			}												    c++;	
 	
 		}   	
