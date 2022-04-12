1e1c7d22fc21dfe3589fb18c33566225349acb53
Simret <simruineb@gmail.com>
2022-04-12 04:07:09 -0700

    Correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include"main.h"
 /**
  * main - Entry point
  * Return:0
@@ -11,21 +11,17 @@ int main(void)
 {
 
 	char c[9] = "_putchar";
-
 	int i = 0;
-
 	while (i < 8)
 
 	{
 
 		_putchar(c[i]);
-
 		i++;
 
 	}
 
 	_putchar('\n');
-
 	return (0);
 
 }
