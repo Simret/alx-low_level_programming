f0ab019da7ee64776b283caec6749c29e14d2759
Simret <simruineb@gmail.com>
2022-04-15 01:25:42 -0700

    Comment out

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,7 +14,6 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
         i = 0;
-
 	/*
 	*while (i < 10)
         *{
@@ -22,6 +21,7 @@ int main(void)
 	*}
 	*/
         printf("Infinite loop avoided! \\o/\n");
-        return (0);
+
+	return (0);
 
 }
