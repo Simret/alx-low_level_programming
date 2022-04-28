c541aed0a81d421613b504f9de9169420d76fe16
Simret <simruineb@gmail.com>
2022-04-28 13:10:11 -0700

    main

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,11 +1,7 @@
 #ifndef MAIN_H
-
 #define MAIN_H
 
-
-
 int _putchar(char c);
-
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -14,5 +10,10 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int square(int n, int val)
 int is_prime_number(int n);
+int last_index(char *s);
+int check(char *s, int start, int end, int pair);
+int is_palindrome(char *s);
+int checker(char *s1, char *s2, int a, int b, int wildUsed);
+int wildcmp(char *s1, char *s2);
 
 #endif
