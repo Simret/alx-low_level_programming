4b6e2594e6c6c9fce7c22086d31fac046f02cf3d
Simret <simruineb@gmail.com>
2022-05-04 18:47:31 -0700

    Concatinate argument

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,6 +7,7 @@
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 int **alloc_grid(int width, int height);
 char *create_array(unsigned int size, char c);
 
