0cdf8338ab81db0eefa2ef64c9b9db2f01936f9f
Simret <simruineb@gmail.com>
2022-05-10 16:13:49 -0700

    Check

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -9,11 +9,11 @@
 int main(void)
 
 {
-       	struct dog my_dog;
+	struct dog my_dog;
 
-	 my_dog.name = "Buffy";
-	 my_dog.age = 2;
-	my_dog.owner = "sara";
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
 	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-        return (0);
+	return (0);
 }
