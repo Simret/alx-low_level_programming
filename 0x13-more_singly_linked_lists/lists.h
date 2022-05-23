7399d63786675c12255663fed5f832e06ac769b0
Simret <simruineb@gmail.com>
2022-05-23 11:51:01 -0700

    Free list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,28 +5,19 @@
 #include <stdio.h>
 
 /**
- *
- *  * struct listint_s - singly linked list
- *
- *   * @n: integer
- *
- *    * @next: points to the next node
- *
- *     *
- *
- *      * Description: singly linked list node structure
- *
- *       * for Holberton project
- *
- *        */
+  * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 
 typedef struct listint_s
 
 {
+	int n;
 
-		int n;
-
-			struct listint_s *next;
+	struct listint_s *next;
 
 } listint_t;
 
@@ -35,5 +26,6 @@ size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
 
 #endif
