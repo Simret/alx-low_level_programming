4b7d45b096373fb45f84abef50a987cfecc0199b
Simret <simruineb@gmail.com>
2022-05-25 08:36:46 -0700

    Number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - Calculate the number of elements.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
+
+size_t listint_len(const listint_t *h)
+
+{
+	const listint_t *t;
+	size_t i = 0;
+
+	t = h;
+	while (t)
+	{
+		i++;
+		t = t->next;
+	}
+	return (i);
+}
