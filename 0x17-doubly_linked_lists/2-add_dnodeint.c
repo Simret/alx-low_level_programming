34294749349e742909e0106e9aa7093d55a0140b
Simret <simruineb@gmail.com>
2022-07-07 17:17:33 -0700

    Add new element

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	
 	
 	if (*head != NULL)
-		(*head)->prev = new; new = new;
-	*head = new; 
+		(*head)->prev = new;
+	*head = new 
 	return (new);
 }
