a747736bd1224dd9926e4fdabbd15a9a6fc814a0
Simret <simruineb@gmail.com>
2022-07-07 18:40:54 -0700

    Add node edited

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -22,6 +22,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newNode->prev = NULL;
 	if (*head != NULL)
 		(*head)->prev = newNode;
-	*head = newNode 
+	*head = newNode; 
 	return (newNode);
 }
