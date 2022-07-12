dc6e559facba76d7f7dec3b277455cafc321d490
Simret <simruineb@gmail.com>
2022-07-11 17:42:29 -0700

    Edited

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -o /tmp/libtest.so https://github.com/simret/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
+wget -P /tmp/ https://raw.github.com/simret/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
 export LD_PRELOAD =/tmp/libtest.so
