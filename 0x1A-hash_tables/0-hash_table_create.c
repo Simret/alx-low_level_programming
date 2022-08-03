771df4bf3459ea76ce3e171af6074d6e2ae616df
Simret <simruineb@gmail.com>
2022-08-03 02:29:47 -0700

    Hash table create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-
 	ht->size = size;
 	
 	head_array = malloc(sizeof(hash_node_t *) * size);
